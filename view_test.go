ca6103d9534d50e6afe59cc17ebc0799bc099b9b
zeMirco <mirco.zeiss@gmail.com>
2014-10-28 19:03:26 +0100

    add tests for complex view queries

--- view_test.go
+++ view_test.go
@@ -18,6 +18,7 @@ type DataDocument struct {
 	Type string `json:"type"`
 	Foo  string `json:"foo"`
 	Beep string `json:"beep"`
+	Age  int    `json:"age"`
 }
 
 func (doc *DataDocument) GetDocument() *Document {
@@ -41,12 +42,34 @@ func TestViewBefore(t *testing.T) {
 	view := make(map[string]string)
 	view["map"] =
 		`
-    function(doc){
-      if (doc.type == 'data') emit(doc.foo)
-    }
-  `
+		function(doc){
+			if (doc.type == 'data') emit(doc.foo)
+		}
+	`
+	// create a bit more comple design document
+	t.Log("creating complex design document...")
+	complexView := make(map[string]string)
+	complexView["map"] =
+		`
+		function(doc) {
+			if (doc.type === 'data') emit([doc.foo, doc.beep])
+		}
+		`
+
+	// create design document with int key
+	t.Log("creating int design document...")
+	intView := make(map[string]string)
+	intView["map"] =
+		`
+		function(doc) {
+			if (doc.type === 'data') emit([doc.foo, doc.age])
+		}
+		`
+
 	views := make(map[string]interface{})
 	views["foo"] = view
+	views["complex"] = complexView
+	views["int"] = intView
 
 	design := &ViewDocument{
 		Document: Document{
@@ -66,6 +89,7 @@ func TestViewBefore(t *testing.T) {
 		Type: "data",
 		Foo:  "foo1",
 		Beep: "beep1",
+		Age:  10,
 	}
 
 	_, err = db_view.Post(doc1)
@@ -77,6 +101,7 @@ func TestViewBefore(t *testing.T) {
 		Type: "data",
 		Foo:  "foo2",
 		Beep: "beep2",
+		Age:  20,
 	}
 
 	_, err = db_view.Post(doc2)
@@ -103,6 +128,54 @@ func TestViewGetWithQueryParameters(t *testing.T) {
 		Key: "foo1",
 	}
 	res, err := view.Get("foo", params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(res.Rows) != 1 {
+		t.Error("view get error")
+	}
+}
+
+func TestViewGetWithStartKeyEndKey(t *testing.T) {
+	view := db_view.View("test")
+
+	startkey := make([]interface{}, 2)
+	startkey[0] = "foo2"
+	startkey[1] = "beep2"
+
+	endkey := make([]interface{}, 2)
+	endkey[0] = "foo2"
+	endkey[1] = "beep2"
+
+	params := QueryParameters{
+		StartKey: startkey,
+		EndKey:   endkey,
+	}
+	res, err := view.Get("complex", params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(res.Rows) != 1 {
+		t.Error("view get error")
+	}
+}
+
+func TestViewGetWithInteger(t *testing.T) {
+	view := db_view.View("test")
+
+	startkey := make([]interface{}, 2)
+	startkey[0] = "foo2"
+	startkey[1] = 20
+
+	endkey := make([]interface{}, 2)
+	endkey[0] = "foo2"
+	endkey[1] = 20
+
+	params := QueryParameters{
+		StartKey: startkey,
+		EndKey:   endkey,
+	}
+	res, err := view.Get("int", params)
 	if err != nil {
 		t.Fatal(err)
 	}
